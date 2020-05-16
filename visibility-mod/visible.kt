086a84f52e4dbe8a5d1a60d77f715eaf7c930482
yoga <abiyoga1335@gmail.com>
2020-05-16 16:47:24 +0700

    visibility modifiers

---
+++ visibility-mod/visible.kt
@@ -0,0 +1,24 @@
+class Animal(private var name: String, private val weight: Double, private val age: Int, private val isMammal: Boolean = true) {
+ 
+    fun getName() : String {
+        return name
+    }
+ 
+    fun setName(newName: String) {
+        name = newName
+    }
+ 
+}
+ 
+fun main() {
+    val dicodingCat = Animal("Dicoding Miaw", 2.5, 2)
+    println(dicodingCat.getName())
+    dicodingCat.setName("Gooose")
+    println(dicodingCat.getName())
+}
+ 
+/*
+output:
+    Dicoding Miaw
+    Gooose
+*/
